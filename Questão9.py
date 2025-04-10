bb54cc0c83024d665797ffcacee32a58699f6b58
Estéfanas Argentina <eacm@discente.ifpe.edu.br>
2025-04-10 17:43:25 -0300

    Create Questão9.py

---
+++ Questão9.py
@@ -0,0 +1,15 @@
+P1 = 1
+P2 = 2
+P3 = 3
+P4 = 4
+n1 = input("Digite o primeiro valor ")
+n1 = float(n1)
+n2 = input("Digite o segundo valor ")
+n2 = float(n2)
+n3 = input("Digite o terceiro valor ")
+n3 = float(n3)
+n4 = input("Digite o quarto valor ")
+n4 = float(n4)
+somapeso = P1 + P2 + P3 + P4
+mediapond = (n1 * P1 + n2 * P2 + n3 * P3 + n4 * P4 )/ somapeso
+print(mediapond)
